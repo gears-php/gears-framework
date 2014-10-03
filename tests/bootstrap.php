c0f764534d48c996c152658884a396af1c2f354c
Denis Krasilnikov <deniskrasilnikov86@gmail.com>
2014-10-03 23:20:28 +0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ use Gears\Framework\Application\ClassLoader;
 
 require_once __DIR__ . '/../src/Application/ClassLoader.php';
 
-ClassLoader::registerNamespaces([
+ClassLoader::registerMappings([
     'Gears\Framework' => __DIR__,
     'Gears\Config' => __DIR__ . '/../component/config/src',
     'Gears\Db' => __DIR__ . '/../component/db/src'
