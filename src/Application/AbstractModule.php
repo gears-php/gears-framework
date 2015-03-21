ea910a429ca20dfa26d6af365b3c31e93f4a7162
Denis Krasilnikov <deniskrasilnikov86@gmail.com>
2015-03-21 21:47:27 +0200

    added doc blocks

--- src/Application/AbstractModule.php
+++ src/Application/AbstractModule.php
@@ -16,32 +16,54 @@ class AbstractModule
      */
     protected $classInfo = null;
 
+    /**
+     * @param Application $app
+     */
     public function __construct(Application $app)
     {
         $this->app = $app;
     }
 
+    /**
+     * Get module configuration file
+     * @return string
+     */
     public function getConfigFile()
     {
         $fileExt = $this->app->getConfig()->getReader()->getFileExt();
         return  $this->getModuleDir() . '/config/module' . $fileExt;
     }
 
+    /**
+     * Register all module class load mappings
+     * @return $this
+     */
     public function register()
     {
         ClassLoader::register($this->getClassInfo()->getNamespaceName(), $this->getModuleDir() . '/src/');
         return $this;
     }
 
+    /**
+     * Custom module loading code
+     */
     public function load()
     {
     }
 
+    /**
+     * Get module base directory
+     * @return string
+     */
     protected function getModuleDir()
     {
         return dirname($this->getClassInfo()->getFileName());
     }
 
+    /**
+     * Return the module class info
+     * @return ReflectionClass
+     */
     protected function getClassInfo()
     {
         if (!$this->classInfo) {
