d67c2fa30e9ee88e4221b4254fa10b89b67ad1d7
dkrasilnikov <deniskrasilnikov86@gmail.com>
2015-04-11 15:51:41 +0000

    removed storage

--- component/storage/README.md
+++
@@ -1,5 +0,0 @@
-gears-storage
-============
-
-A general-purpose storage solution used in Gears Framework. Suitable for reading and storing application configuration 
-or any other kind of runtime data in a simple and easily manageable manner.
--- component/storage/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../src/Reader/Exception/FileNotFound.php';
-require_once __DIR__ . '/../src/Reader/ReaderAbstract.php';
-require_once __DIR__ . '/../src/Reader/Yaml.php';
-require_once __DIR__ . '/../src/Storage.php';
