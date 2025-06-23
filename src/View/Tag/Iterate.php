749c19695497b9f3931c417c83a3f7ff11bbb594
Denis Krasilnikov <deniskrasilnikov86@gmail.com>
2025-06-23 17:18:59 +0300

    template engine evo

--- src/View/Tag/Iterate.php
+++ src/View/Tag/Iterate.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Gears\Framework\View\Tag;
 
+use Gears\Framework\View\RenderingException;
+
 /**
  * @noinspection PhpUnused
  */
@@ -15,9 +17,20 @@ final class Iterate extends AbstractTag
     public function processNode(array $node): void
     {
         $sourceVar = key($node['attrs']);
+        $sourceCollection = $this->template->getVar($sourceVar);
+        if (!is_iterable($sourceCollection)) {
+            throw new RenderingException(
+                sprintf(
+                    'Template variable "%s" is not iterable. Defined in %s:%d',
+                    $sourceVar,
+                    $this->template->getFilePath(),
+                    $node['tag_pos'][0]
+                )
+            );
+        }
         $destVar = current($node['attrs']);
         $backupVar = $this->template->getVar($destVar);
-        foreach ($this->template->getVar($sourceVar) as $value) {
+        foreach ($sourceCollection as $value) {
             $this->template->setVar($destVar, $value);
             parent::processNode($node);
         }
